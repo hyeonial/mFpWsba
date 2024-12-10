81ace6114ee1e30c0e5dd1376f58b5e478a295a0
hyeonial <seankim3162@naver.com>
2024-12-10 20:09:37 +0900

    re

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function checkPassword() {
     const password = document.getElementById("password").value;
-    const correctPassword = "mFpWsba"; // 올바른 암호
+    const correctPassword = "4792"; // 올바른 암호
 
     if (password === correctPassword) {
-        alert("암호가 맞습니다! 관리자 페이지로 이동합니다.");
+        alert("correct answer. See you next level :D");
         // 다음 페이지로 이동하거나 관리자를 위한 다른 페이지로 이동하는 코드 추가
     } else {
-        alert("암호가 틀렸습니다. 다시 시도해 주세요.");
+        alert("The wrong password");
     }
 }
